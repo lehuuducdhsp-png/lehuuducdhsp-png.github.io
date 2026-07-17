041b293c60007de04248b6dfa9ef4c49d22678a9
lehuuducdhsp-png <lehuuducdhsp@gmail.com>
2026-07-17 15:04:48 +0700

    Thêm lọc lịch sử thu theo học sinh

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME='duc-classroom-2026.07.17.11';
+const CACHE_NAME='duc-classroom-2026.07.17.12';
 const LOCAL_CORE=[
   './',
   './index.html',
-  './tuition-poster.js?v=2026.07.17.11',
+  './tuition-poster.js?v=2026.07.17.12',
   './student/',
   './student/index.html',
   './student/manifest.webmanifest',
